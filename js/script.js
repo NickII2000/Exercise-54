4743969930feafc5267b9edfee277c56ddba41b0
Nick <Nick_II2000@mail.ru>
2022-03-26 21:16:28 +0600

    15-th commit

--- js/script.js
+++ js/script.js
@@ -40,10 +40,10 @@ window.addEventListener('DOMContentLoaded', () => {
     });
 
     // Timer
-    const deadline = '2022-03-27';
+    // const deadline = '2022-03-27';
     // const deadline = '2022-05-09';
     // const deadline = new Date(new Date().getTime() + 7000);
-    // const deadline = new Date(new Date().getTime() + 10 * (24 * 60 * 60 * 1000));
+    const deadline = new Date(new Date().getTime() + 10 * (24 * 60 * 60 * 1000));
 
     function endTimeRemaining(endTime = new Date()) {
         const t = Date.parse(endTime) - Date.parse(new Date());
